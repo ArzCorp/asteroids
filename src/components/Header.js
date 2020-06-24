e3855a6eebd4ffba7f5d83bc8b89b993cfc44ca9
Osvaldo Arzate <osvarz.new@gmail.com>
2020-06-24 12:48:53 -0500

    Se agregar estilos globales

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,14 @@ import iconFav from '../images/icon-fav.svg';
 class Header extends React.Component {
   render() {
     return (
-      <header className="header__container">
-        <img src={Logo} className="header__logo" alt="Logo" />
-        <div className="navbar">
-          <a className="header__ancore" href="/">Asteroides</a>
-          <a className="header__ancore" href="/">Galeria</a>
-          <a className="header__ancore" href="/"><img className="icon-fav" alt="icono favoritos" src={iconFav} /></a>
+      <header className="header">
+        <div className="container">
+          <img src={Logo} className="header__logo" alt="Logo" />
+          <div className="navbar">
+            <a className="header__ancore" href="/">Asteroides</a>
+            <a className="header__ancore" href="/">Galeria</a>
+            <a className="header__ancore" href="/"><img className="icon-fav" alt="icono favoritos" src={iconFav} /></a>
+          </div>
         </div>
       </header>
     )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header.js'
+import './css/Global.css'
 
 const body = document.getElementById('root');
 ReactDOM.render(<Header />, body);
