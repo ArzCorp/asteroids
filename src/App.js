c2bdbfbab7dacdf8595d405738eacbae586dc0ab
Luis Cabezas <luiscabezas25@hotmail.com>
2020-07-20 11:44:50 -0500

    Cambios y ajustes de styles

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import Principal from './pages/Principal';
 import Asteroids from './pages/Asteroids';
+import Cargando from './pages/Cargando';
+import Galeria from './pages/Galeria';
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
-        <Route exact path="/" component={Principal} />
-        <Route exact path="/asteroides" component={Asteroids} />
+        <div className="prueba">
+          <Route exact path="/" component={Principal} />
+          <Route exact path="/asteroides" component={Asteroids} />
+          <Route exact path="/favoritos" component={Cargando} />
+          <Route exact path="/galeria" component={Galeria} />
+        </div>
       </Layout>
     </BrowserRouter>
   );
--- src/pages/Asteroids.js
+++ src/pages/Asteroids.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import Card from '../components/Card'
 import Video from '../components/Video';
 import asteroids from '../images/asteroid.svg';
 import './css/asteroides.css';
@@ -10,18 +9,11 @@ const Asteroids = () => {
     <div className="asteroids">
       <Video />
       <div className="orbit__asteroids">
-        <img className="asteroid1" src={asteroids} alt="asteroide" />
+          <img className="asteroid1 info" src={asteroids} alt="asteroide" />
         <img className="asteroid2" src={asteroids} alt="asteroide" />
         <img className="asteroid3" src={asteroids} alt="asteroide" />
         <img className="asteroid4" src={asteroids} alt="asteroide" />
-        <Link to="/" className="button-asteroids">Mas asteroides</Link>
-      </div>
-      <div className="container">
-        <div className="content__asteroids">
-          <Card />
-          <Card />
-          <Card />
-        </div>
+        <Link to="/galeria" className="button-asteroids">Mas asteroides</Link>
       </div>
     </div>
   );
