831c56b0644a41314e237ef8c3a9c7c39be936f6
Osvaldo Arzate <osvarz.new@gmail.com>
2020-07-25 11:35:19 -0500

    Se termina estructura y estilos

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Principal from './pages/Principal';
 import Asteroids from './pages/Asteroids';
 import Cargando from './pages/Cargando';
-import Galeria from './pages/Galeria';
+import Galery from './pages/Galery';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           <Route exact path="/" component={Principal} />
           <Route exact path="/asteroides" component={Asteroids} />
           <Route exact path="/favoritos" component={Cargando} />
-          <Route exact path="/galeria" component={Galeria} />
+          <Route exact path="/galeria" component={Galery} />
         </div>
       </Layout>
     </BrowserRouter>
--- src/components/Card.js
+++ src/components/Card.js
@@ -24,142 +24,142 @@ class Card extends React.Component {
       this.setState({
         data: [
           {
-            "neo_reference_id":"2021277",
-            "image_url":"asteroid_15123468.jpg",
+            "neo_reference_id": "2021277",
+            "image_url": "asteroid_15123468.jpg",
             "name": "21277 (1996 TO5)",
-            "absolute_magnitude_h":16.1,
-            "estimated_diameter":{
-                "kilometers":{
-                    "estimated_diameter_min":1.6016033798,
-                    "estimated_diameter_max":3.5812940302
-                }
-            },
-            "is_potentially_hazardous_asteroid":false,
-            "close_approach_data":[{
-                "close_approach_date": "2020-07-01",
-                "epoch_date_close_approach":-775272300000,
-                "relative_velocity":{
-                    "kilometers_per_hour":"55834.1106763388"
-                },
-                "miss_distance":{
-                    "kilometers":"5000054.084456751"
-                }
-            }],
-            "orbital_data":{
-                "orbit_determination_date":"2020-04-29 06:55:15",
-                "minimum_orbit_intersection":".313031",
-                "eccentricity":".5205881322259853",
-                "semi_major_axis":"2.37660147442171",
-                "inclination":"20.95226489770891",
-                "orbital_period":"1338.23680580907",
-                "perihelion_distance":"1.139370951806989",
-                "aphelion_distance":"3.613831997036431",
-                "orbit_class":{
-                    "orbit_class_type":"AMO",
-                    "orbit_class_description":"Near-Earth asteroid orbits similar to that of 1221 Amor",
-                    "orbit_class_range":"1.017 AU < q (perihelion) < 1.3 AU"
-                }
-            }
-        },
-        {
-          "neo_reference_id":"3726710",
-          "image_url":"Asteroide1.jpg",
-          "name":"(2015 RC)",
-          "absolute_magnitude_h":24.3,
-          "estimated_diameter":{
-              "kilometers":{
-                  "estimated_diameter_min":0.0366906138,
-                  "estimated_diameter_max":0.0820427065
+            "absolute_magnitude_h": 16.1,
+            "estimated_diameter": {
+              "kilometers": {
+                "estimated_diameter_min": 1.6016033798,
+                "estimated_diameter_max": 3.5812940302
               }
-          },
-          "is_potentially_hazardous_asteroid":false,
-          "close_approach_data":[{
+            },
+            "is_potentially_hazardous_asteroid": false,
+            "close_approach_data": [{
               "close_approach_date": "2020-07-01",
-              "epoch_date_close_approach":1441705500000,
-              "relative_velocity":{
-                  "kilometers_per_hour":"70146.106302123"
+              "epoch_date_close_approach": -775272300000,
+              "relative_velocity": {
+                "kilometers_per_hour": "55834.1106763388"
               },
-              "miss_distance":{
-                  "kilometers":"4027630.320552233"
+              "miss_distance": {
+                "kilometers": "5000054.084456751"
               }
-          }]
-      },
-      {
-          "neo_reference_id":"2162038",
-          "name":"162038 (1996 DH)",
-          "image_url":"PIA02494_orig.jpg",
-          "absolute_magnitude_h":16.6,
-          "estimated_diameter":{
-              "kilometers":{
-                  "estimated_diameter_min":1.2721987854,
-                  "estimated_diameter_max":2.844722965
+            }],
+            "orbital_data": {
+              "orbit_determination_date": "2020-04-29 06:55:15",
+              "minimum_orbit_intersection": ".313031",
+              "eccentricity": ".5205881322259853",
+              "semi_major_axis": "2.37660147442171",
+              "inclination": "20.95226489770891",
+              "orbital_period": "1338.23680580907",
+              "perihelion_distance": "1.139370951806989",
+              "aphelion_distance": "3.613831997036431",
+              "orbit_class": {
+                "orbit_class_type": "AMO",
+                "orbit_class_description": "Near-Earth asteroid orbits similar to that of 1221 Amor",
+                "orbit_class_range": "1.017 AU < q (perihelion) < 1.3 AU"
               }
+            }
           },
-          "is_potentially_hazardous_asteroid":false,
-          "close_approach_data":[{
-              "close_approach_date": "2020-07-02",
-              "epoch_date_close_approach":1441705500000,
-              "relative_velocity":{
-                  "kilometers_per_hour":"70146.106302123"
-              },
-              "miss_distance":{
-                  "kilometers":"4027630.320552233"
+          {
+            "neo_reference_id": "3726710",
+            "image_url": "Asteroide1.jpg",
+            "name": "(2015 RC)",
+            "absolute_magnitude_h": 24.3,
+            "estimated_diameter": {
+              "kilometers": {
+                "estimated_diameter_min": 0.0366906138,
+                "estimated_diameter_max": 0.0820427065
               }
-          }],
-          "orbital_data":{
-              "orbit_determination_date":"2020-05-26 08:20:27",
-              "minimum_orbit_intersection":".135023",
-              "eccentricity":".2766900137323586",
-              "semi_major_axis":"1.586813070911491",
-              "inclination":"17.23326680059163",
-              "orbital_period":"730.107955454301",
-              "perihelion_distance":"1.147757740530304",
-              "aphelion_distance":"2.025868401292677",
-              "orbit_class":{
-                  "orbit_class_type":"AMO",
-                  "orbit_class_description":"Near-Earth asteroid orbits similar to that of 1221 Amor",
-                  "orbit_class_range":"1.017 AU < q (perihelion) < 1.3 AU"
-              }
-          }
-      },
-      {
-        "neo_reference_id":"2162038",
-        "name":"162038 (1996 DH)",
-        "image_url":"PIA02494_orig.jpg",
-        "absolute_magnitude_h":16.6,
-        "estimated_diameter":{
-            "kilometers":{
-                "estimated_diameter_min":1.2721987854,
-                "estimated_diameter_max":2.844722965
-            }
-        },
-        "is_potentially_hazardous_asteroid":false,
-        "close_approach_data":[{
-            "close_approach_date": "2020-07-02",
-            "epoch_date_close_approach":1441705500000,
-            "relative_velocity":{
-                "kilometers_per_hour":"70146.106302123"
             },
-            "miss_distance":{
-                "kilometers":"4027630.320552233"
+            "is_potentially_hazardous_asteroid": false,
+            "close_approach_data": [{
+              "close_approach_date": "2020-07-01",
+              "epoch_date_close_approach": 1441705500000,
+              "relative_velocity": {
+                "kilometers_per_hour": "70146.106302123"
+              },
+              "miss_distance": {
+                "kilometers": "4027630.320552233"
+              }
+            }]
+          },
+          {
+            "neo_reference_id": "2162038",
+            "name": "162038 (1996 DH)",
+            "image_url": "PIA02494_orig.jpg",
+            "absolute_magnitude_h": 16.6,
+            "estimated_diameter": {
+              "kilometers": {
+                "estimated_diameter_min": 1.2721987854,
+                "estimated_diameter_max": 2.844722965
+              }
+            },
+            "is_potentially_hazardous_asteroid": false,
+            "close_approach_data": [{
+              "close_approach_date": "2020-07-02",
+              "epoch_date_close_approach": 1441705500000,
+              "relative_velocity": {
+                "kilometers_per_hour": "70146.106302123"
+              },
+              "miss_distance": {
+                "kilometers": "4027630.320552233"
+              }
+            }],
+            "orbital_data": {
+              "orbit_determination_date": "2020-05-26 08:20:27",
+              "minimum_orbit_intersection": ".135023",
+              "eccentricity": ".2766900137323586",
+              "semi_major_axis": "1.586813070911491",
+              "inclination": "17.23326680059163",
+              "orbital_period": "730.107955454301",
+              "perihelion_distance": "1.147757740530304",
+              "aphelion_distance": "2.025868401292677",
+              "orbit_class": {
+                "orbit_class_type": "AMO",
+                "orbit_class_description": "Near-Earth asteroid orbits similar to that of 1221 Amor",
+                "orbit_class_range": "1.017 AU < q (perihelion) < 1.3 AU"
+              }
             }
-        }],
-        "orbital_data":{
-            "orbit_determination_date":"2020-05-26 08:20:27",
-            "minimum_orbit_intersection":".135023",
-            "eccentricity":".2766900137323586",
-            "semi_major_axis":"1.586813070911491",
-            "inclination":"17.23326680059163",
-            "orbital_period":"730.107955454301",
-            "perihelion_distance":"1.147757740530304",
-            "aphelion_distance":"2.025868401292677",
-            "orbit_class":{
-                "orbit_class_type":"AMO",
-                "orbit_class_description":"Near-Earth asteroid orbits similar to that of 1221 Amor",
-                "orbit_class_range":"1.017 AU < q (perihelion) < 1.3 AU"
+          },
+          {
+            "neo_reference_id": "2162038",
+            "name": "162038 (1996 DH)",
+            "image_url": "PIA02494_orig.jpg",
+            "absolute_magnitude_h": 16.6,
+            "estimated_diameter": {
+              "kilometers": {
+                "estimated_diameter_min": 1.2721987854,
+                "estimated_diameter_max": 2.844722965
+              }
+            },
+            "is_potentially_hazardous_asteroid": false,
+            "close_approach_data": [{
+              "close_approach_date": "2020-07-02",
+              "epoch_date_close_approach": 1441705500000,
+              "relative_velocity": {
+                "kilometers_per_hour": "70146.106302123"
+              },
+              "miss_distance": {
+                "kilometers": "4027630.320552233"
+              }
+            }],
+            "orbital_data": {
+              "orbit_determination_date": "2020-05-26 08:20:27",
+              "minimum_orbit_intersection": ".135023",
+              "eccentricity": ".2766900137323586",
+              "semi_major_axis": "1.586813070911491",
+              "inclination": "17.23326680059163",
+              "orbital_period": "730.107955454301",
+              "perihelion_distance": "1.147757740530304",
+              "aphelion_distance": "2.025868401292677",
+              "orbit_class": {
+                "orbit_class_type": "AMO",
+                "orbit_class_description": "Near-Earth asteroid orbits similar to that of 1221 Amor",
+                "orbit_class_range": "1.017 AU < q (perihelion) < 1.3 AU"
+              }
             }
-        }
-    },
+          },
         ],
       });
     }, 100);
@@ -167,30 +167,30 @@ class Card extends React.Component {
 
   ponerDatos = () => (
     this.state.data.map((nombre) => (
-          <div  key={ nombre.neo_reference_id }>
-          <div className="card">
-      <div className="asteroid">
-        <img src={asteroid} alt="asteroide card" />
-      </div>
-      <div className="card__content">
-          <div>
-            <h3 className="card__name" >
-            { nombre.name }
-            </h3>
+      <div key={nombre.neo_reference_id}>
+        <div className="card">
+          <div className="asteroid">
+            <img src={asteroid} alt="asteroide card" />
+          </div>
+          <div className="card__content">
+            <div>
+              <h3 className="card__name" >
+                {nombre.name}
+              </h3>
             </div>
             <div className="card__container">
               <div className="card__item">
-                <img src={asteroids} alt=""/>
+                <img src={asteroids} alt="" />
               </div>
               <div className="card__item">
                 <div className="data">
-                  <h3  className="title__api">Diametro estimado</h3>
-                  <p className="title__data">{ nombre.estimated_diameter.kilometers.estimated_diameter_min }</p>
-                  <p className="title__data">{ nombre.estimated_diameter.kilometers.estimated_diameter_max }</p>
+                  <h3 className="title__api">Diametro estimado</h3>
+                  <p className="title__data">{nombre.estimated_diameter.kilometers.estimated_diameter_min}</p>
+                  <p className="title__data">{nombre.estimated_diameter.kilometers.estimated_diameter_max}</p>
                 </div>
               </div>
               <div className="card__item">
-                <img src={orbit} alt=""/>
+                <img src={orbit} alt="" />
               </div>
               <div className="card__item">
                 <div className="data">
@@ -200,7 +200,7 @@ class Card extends React.Component {
                 </div>
               </div>
               <div className="card__item">
-                <img src={telescope} alt=""/>
+                <img src={telescope} alt="" />
               </div>
               <div className="card__item">
                 <div className="data">
@@ -210,32 +210,33 @@ class Card extends React.Component {
                 </div>
               </div>
               <div className="card__item">
-                <img src={alert} alt=""/>
+                <img src={alert} alt="" />
               </div>
               <div className="card__item">
                 <div className="data">
-                  <h3  className="title__api">Potencialmente Peligroso</h3>
+                  <h3 className="title__api">Potencialmente Peligroso</h3>
                   <p className="title__data">dfdf</p>
                   <p className="title__data">dfdf</p>
                 </div>
               </div>
             </div>
             {/* <Link to="/" className="card__button" alt="">Explora</Link> */}
-            </div>
-    </div>
           </div>
+        </div>
+      </div>
     ))
   )
 
   render() {
     console.log('2. render()');
     console.log(this.state.data)
-      return (
+    return (
       <div className="father">
-      { this.ponerDatos() }
+        {this.ponerDatos()}
       </div>
-    )}
+    )
   }
+}
 
 
 export default Card;
